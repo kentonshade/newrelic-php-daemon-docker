6ea47be2c34a9982bda8dee75425cc798d68e837
Kenton Shade, Lead Workflow Engineer <kentonshade@gmail.com>
2022-07-15 10:50:12 -0400

    NR-6490: Reinstating git commands
    https://issues.newrelic.com/browse/NR-6490
    
    [Ticket: https://issues.newrelic.com/browse/NR-6490]

--- tools/update-docker-image.bash
+++ tools/update-docker-image.bash
@@ -9,21 +9,20 @@ fi
 version=$1
 version_short=$(echo $1 | awk -F "." '{print $1 "." $2 "." $3}')
 
-wget -O /tmp/newrelic-php5-${version}-linux-musl.tar.gz https://download.newrelic.com/php_agent/archive/${version}/newrelic-php5-${version}-linux-musl.tar.gz;
+wget -O /tmp/newrelic-php5-"${version}"-linux-musl.tar.gz https://download.newrelic.com/php_agent/archive/"${version}"/newrelic-php5-"${version}"-linux-musl.tar.gz;
 
 sha=$(sha256sum /tmp/newrelic-php5-${version}-linux-musl.tar.gz | awk '{print $1}')
 
-mkdir $version_short
+mkdir "${version_short}"
 
-cp docker-entrypoint-template $version_short/docker-entrypoint.sh
-cd $version_short
+cp docker-entrypoint-template "${version_short}"/docker-entrypoint.sh
+cd "${version_short}"
 
 sed \
    -e s/"ENV[[:space:]]NEWRELIC_VERSION/ENV NEWRELIC_VERSION ${version}"/ \
    -e s/"ENV[[:space:]]NEWRELIC_SHA/ENV NEWRELIC_SHA ${sha}"/ \
     ../Dockerfile-template > Dockerfile
 
-# GHA: commenting out to test the more dynamic action option
-# git checkout -b $version_short
-# git add .
-# git commit -m "version bump to ${version_short}"
+git checkout -b "${version_short}"
+git add .
+git commit -m "version bump to ${version_short}"
